5d8bc9f3f5bffa45160bc80397505525385b7fd6
webbhm <webb.howard@gmail.com>
2017-08-04 14:14:36 -0400

    Add files via upload

---
+++ getImage.py
@@ -0,0 +1,20 @@
+import picamera
+import time
+
+
+#Use this line for MVP 1.0
+dir = '/home/pi/Documents/OpenAg-MVP/webcam/'
+#Use this line for MVP 2.0
+# dir = '/home/pi/MVP/pictures/'
+
+#Create file name with timestamp
+fileName=dir + time.strftime("%Y-%m-%d-%H%M%S") + '.jpg'
+# print(fileName)
+camera = picamera.PiCamera()
+
+# Adjust brightness and ISO for lighting
+camera.brightness=50
+camera.ISO=0
+
+# Capture an image
+camera.capture(fileName)
